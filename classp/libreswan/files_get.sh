5cee6c676209926cff5bc724cc0a2e7cabe9c32c
Pierre Veelen <pierre@pvln.nl>
2020-02-19 13:25:11 +0100

    folders corrected

--- classp/libreswan/files_get.sh
+++ classp/libreswan/files_get.sh
@@ -7,5 +7,10 @@ sudo cp /etc/ipsec.conf ipsec.conf
 #get specifiec ipsec configuration file
 sudo cp /etc/ipsec.d/RPI4-to-ClassA.conf RPI4-to-ClassA.conf
 sudo cp /etc/ipsec.d/RPI4-to-ClassA.secrets RPI4-to-ClassA.secrets
+sudo cp /etc/ipsec.conf ipsec.conf
+
+#change ownership
+sudo chown pi:pi RPI4-to-ClassA.*
+sudo chown pi:pi ipsec.conf
 
 #DONE
--- classp/libreswan/files_set.sh
+++ classp/libreswan/files_set.sh
@@ -1,14 +1,15 @@
 #!/bin/bash
 
-cd ~/vpn/classp/libreswan
+cd ~/vpn/conf/classp/libreswan
 #set ipsec baseline configuration file
 sudo cp ipsec.conf /etc/ipsec.conf
 
 #set specific ipsec configuration file
-sudo cp conn_P-L.conf /etc/ipsec.d/conn_P-L.conf
+sudo cp RPI4-to-ClassA.conf /etc/ipsec.d/RPI4-to-ClassA.conf
+sudo cp RPI4-to-ClassA.secrets /etc/ipsec.d/RPI4-to-ClassA.secrets
 
 #set scripts to execution mode
-sudo chmod +x ~/vpn/classp/libreswan/*.sh
+sudo chmod +x ~/vpn/conf/classp/libreswan/*.sh
 
 sudo cp sysctl.conf /etc/sysctl.conf
 
