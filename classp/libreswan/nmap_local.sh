505446b4a4a7d34c351ba06a0f137bc16d142397
Pierre Veelen <pierre@pvln.nl>
2020-02-19 17:01:44 +0100

    settings added

--- classp/libreswan/nmap_local.sh
+++ classp/libreswan/nmap_local.sh
@@ -1,3 +1,10 @@
 #!/bin/bash
 #
-sudo nmap -p 22,500,4500 86.81.35.18 -Pn -sU -sT
+# set IP address values
+# Insipration: https://askubuntu.com/questions/743493/best-way-to-read-a-config-file-in-bash
+. settings
+#
+#
+sudo nmap -p 22,500,4500 $classpIP -Pn -sU -sT
+
+#DONE
