6c5e2bed2e9ca2af85f5f173cc7c697577044f71
Pierre Veelen <pierre@pvln.nl>
2020-02-20 09:50:37 +0100

    updated to new folder

--- classa/openswan/get_it.sh
+++ classa/openswan/get_it.sh
@@ -1,24 +1,24 @@
 #!/bin/bash
 
 #get ipsec baseline configuration file
-wget -nv https://raw.githubusercontent.com/pierre-pvln/conf-test/master/a/ipsec.conf -O ~/vpn/a/ipsec.conf
+wget -nv https://raw.githubusercontent.com/pierre-pvln/conf/master/classa/openswan/ipsec.conf -O ~/vpn/conf/classa/openswan/ipsec.conf
 
 #get specific ipsec configuration file
-wget -nv https://raw.githubusercontent.com/pierre-pvln/conf-test/master/a/conn_A-L.conf -O ~/vpn/a/conn_A-L.conf
-wget -nv https://raw.githubusercontent.com/pierre-pvln/conf-test/master/a/conn_A-P.conf -O ~/vpn/a/conn_A-P.conf
+wget -nv https://raw.githubusercontent.com/pierre-pvln/conf/master/classa/openswan/ClassA-to-RPI4.conf -O ~/vpn/conf/classa/openswan/ClassA-to-RPI4.conf
+#wget -nv https://raw.githubusercontent.com/pierre-pvln/conf/master/classa/openswan/ClassB-to-RPI4.conf -O ~/vpn/conf/classa/openswan/ClassB-to-RPI4.conf
 
 #get initialization script
-wget -nv https://raw.githubusercontent.com/pierre-pvln/conf-test/master/a/init.sh -O ~/vpn/a/init.sh
+wget -nv https://raw.githubusercontent.com/pierre-pvln/conf/master/classa/openswan/init.sh -O ~/vpn/conf/classa/openswan/init.sh
 
 # get override configuration
-wget -nv https://raw.githubusercontent.com/pierre-pvln/conf-test/master/a/90-override.conf -O ~/vpn/a/90-override.conf
+#wget -nv https://raw.githubusercontent.com/pierre-pvln/conf/master/classa/openswan/90-override.conf -O ~/vpn/conf/classa/openswan/90-override.conf
 
 #get start script
-wget -nv https://raw.githubusercontent.com/pierre-pvln/conf-test/master/a/start.sh -O ~/vpn/a/start.sh
+wget -nv https://raw.githubusercontent.com/pierre-pvln/conf/master/classa/openswan/start.sh -O ~/vpn/conf/classa/openswan/start.sh
 
-wget -nv https://raw.githubusercontent.com/pierre-pvln/conf-test/master/a/files_get.sh -O ~/vpn/a/files_get.sh
-wget -nv https://raw.githubusercontent.com/pierre-pvln/conf-test/master/a/files_set.sh -O ~/vpn/a/files_set.sh
+wget -nv https://raw.githubusercontent.com/pierre-pvln/conf/master/classa/openswan/files_get.sh -O ~/vpn/conf/classa/openswan/files_get.sh
+wget -nv https://raw.githubusercontent.com/pierre-pvln/conf/master/classa/openswan/files_set.sh -O ~/vpn/conf/classa/openswan/files_set.sh
 
-chmod +x ~/vpn/a/*.sh
+chmod +x ~/vpn/conf/classa/openswan/*.sh
 
 #DONE
